844465c322da54411915837caeb9430e04046619
Pranav Narravula <pranavnarravula@gmail.com>
2025-04-14 22:45:15 -0400

    Added routing for proper API requests.

--- server/server.js
+++ server/server.js
@@ -3,20 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const userdataRoutes = require('./routes/userdata');
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+app.use(express.json())
+
 app.use((req,res,next)=>{
     console.log(req.path, req.method);
     next();
 })
 
-app.get('/', (req, res) => {
-    res.json({mssg: 'Hello World!'});
-})
+//routes
+app.use('/api/userdata', userdataRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on port ', process.env.PORT);
