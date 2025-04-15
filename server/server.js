96b5d0a3b4e18d6290bd8ed5f0fa88d2c9fe97cf
Pranav Narravula <pranavnarravula@gmail.com>
2025-04-14 20:26:12 -0400

    Updated server.js and package files, might be some issues with repeat package.json and package-lock.json.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({mssg: 'Hello World!'});
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port ', process.env.PORT);
+})
+
+process.env
+
+
 
