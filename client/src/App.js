62301f4ff422f90002d0dce0de9e8dbdbb130c8c
Pranav Narravula <pranavnarravula@gmail.com>
2025-04-22 08:05:00 -0400

    Created pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import LogPage from './pages/LogPage';
+import LogPage from './pages/LogPage'
+import Navbar from './components/Navbar'
+import AdminPage from './pages/AdminPage';
+import SignInPage from './pages/SignInPage';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route 
-              path="/"
+              path="/logpage"
               element={<LogPage />}
+            />
+            <Route
+              path="/adminpage"
+              element={<AdminPage />}
+            />
+            <Route 
+              path="/"
+              element={<SignInPage />}
             />
           </Routes>
         </div>
---
+++ client/src/pages/AdminPage.js
@@ -0,0 +1,9 @@
+const AdminPage = () => {
+    return (
+        <div className="LogPage">
+            <h2>AdminPage</h2>
+        </div>
+    )
+}
+
+export default AdminPage
